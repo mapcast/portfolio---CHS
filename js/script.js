fc175438897c455865b70caad9cb61645b06df4b
mapcast <odri0220@naver.com>
2020-07-26 18:23:29 +0900

    완성본 1

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function openChatting1() {
+  url = "http://busanit4033.cafe24.com/chatting.jsp?userNum=33&opponentNum=44";
+  window.open(
+    url,
+    "chat1",
+    "width=1000, height=601, scrollbars=no, location=no, toobar=no, menubar=no"
+  );
+}
+function openChatting2() {
+  url = "http://busanit4033.cafe24.com/chatting.jsp?userNum=44&opponentNum=33";
+  window.open(
+    url,
+    "chat2",
+    "width=1000, height=601, top=200, left=500, scrollbars=no, location=no, toobar=no, menubar=no"
+  );
+}
